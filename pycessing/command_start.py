97bd2fa8d2b74a4b34754e74e02404723b91572c
Takashi Kitao <takashi.kitao@gmail.com>
2015-03-15 22:58:14 +0900

    Added an option for Jython

--- pycessing/command_start.py
+++ pycessing/command_start.py
@@ -6,9 +6,10 @@ import settings
 
 
 def start():
+    java_opts = '-Dpython.cachedir.skip=false'
     jython_file = os.path.join(settings.COMMAND_LIB, 'jython.jar')
     runner_file = os.path.join(settings.COMMAND_PACKAGE, 'sketch_runner.py')
     command_args = ' '.join(sys.argv[1:])
 
-    subprocess.call('java -jar {0} {1} {2}'.format(
-        jython_file, runner_file, command_args), shell=True)
+    subprocess.call('java {0} -jar {1} {2} {3}'.format(
+        java_opts, jython_file, runner_file, command_args), shell=True)
